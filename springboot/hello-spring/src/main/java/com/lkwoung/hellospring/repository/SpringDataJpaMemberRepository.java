8eb80f697ab078f7e2e5b6c958ee7208b6fa3ccd
KiWoungLEE <whrjzm123@gmail.com>
2022-07-20 17:51:11 +0900

    add DB content

--- springboot/hello-spring/src/main/java/com/lkwoung/hellospring/repository/SpringDataJpaMemberRepository.java
+++ springboot/hello-spring/src/main/java/com/lkwoung/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -8,6 +8,8 @@ import java.util.Optional;
 //JPA repository를 가지고 있으니까 spring이 알아서 등록해줌
 
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+    //이름의 규칙을 보고 sql를 짜줌
+    //JPQL -> select m from Member m where m.name = ?
     @Override
     Optional<Member> findByName(String name);
 }
